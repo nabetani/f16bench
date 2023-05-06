efb53ee74bdf8a282975332700601f13465d9dac
Nabetani <takenori@nabetani.sakura.ne.jp>
2023-05-06 16:14:58 +0900

    clang で x64バイナリ作る

--- run.sh
+++ run.sh
@@ -7,6 +7,12 @@ echo "clang_o3" >> result.txt
 bin/clang_o3 >> result.txt
 echo "clang_o2" >> result.txt
 bin/clang_o2 >> result.txt
+
+echo "clang_o3 (rosetta)" >> result.txt
+bin/iclang_o3 >> result.txt
+echo "clang_o2 (rosetta)" >> result.txt
+bin/iclang_o2 >> result.txt
+
 echo "gcc_o3" >> result.txt
 bin/gcc_o3 >> result.txt
 echo "gcc_o2" >> result.txt
